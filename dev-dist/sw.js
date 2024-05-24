80f0216fd3e167b405722d0ba23fc9c5522da634
MarlaRey <hejmarlenehej@gmail.com>
2024-05-24 13:40:30 +0200

    sw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4hpmb4p5aug"
+    "revision": "0.010rf108fvo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
