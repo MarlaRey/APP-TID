3fad0412f057ec4a954897868c015c1175c534a9
MarlaRey <hejmarlenehej@gmail.com>
2024-05-24 13:30:11 +0200

    logo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.80pb84ibihg"
+    "revision": "0.4hpmb4p5aug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
